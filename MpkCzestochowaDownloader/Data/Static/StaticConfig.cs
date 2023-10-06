18b3b7c233783de31236731c216fcc529997832a
chkam05 <kamil.k3@outlook.com>
2023-10-06 15:10:51 +0200

    Got URLS for next requests

--- MpkCzestochowaDownloader/Data/Static/StaticConfig.cs
+++ MpkCzestochowaDownloader/Data/Static/StaticConfig.cs
@@ -13,6 +13,8 @@ namespace MpkCzestochowaDownloader.Data.Static
 
         public static readonly string BaseURL = @"https://www.czestochowa.pl/";
         public static readonly string LinesURL = @"https://www.czestochowa.pl/rozklady-jazdy";
+        public static readonly string DeparturesURL = @"https://www.czestochowa.pl/rozklady-jazdy?linia=1&przystanek=70-FIELDORFA-NILA&kierunek=0-KUCELIN&trasa=1490";
+        public static readonly string ArrivesURL = @"https://www.czestochowa.pl/timetables/trip/stops?t(data-trip)=4236&ft(data-time)=04:33:00&dt(date)=2023-10-06";
 
     }
 }
